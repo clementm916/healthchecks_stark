6f6ed90066408de4bde14dffaf1e270009b4c267
clementm916 <clementm916@gmail.com>
2016-12-06 19:45:46 +0300

    solving url error

--- hc/urls.py
+++ hc/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^accounts/', include('hc.accounts.urls')),
     url(r'^', include('hc.api.urls')),
     url(r'^', include('hc.front.urls')),
-    url(r'^', include('hc.payments.urls'))
+    url(r'^', include('hc.payments.urls')),
     url(r'^', include('favicon.urls')),
 ]
